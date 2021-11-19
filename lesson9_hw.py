b6f688292d6f52897b3c65d0aea08496417cbf80
fedtat <fedorova.int@gmail.com>
2021-11-19 17:58:13 +0200

    updated homework to lesson #9

--- lesson9_hw.py
+++ lesson9_hw.py
@@ -6,26 +6,23 @@ from datetime import datetime
 class FilesWorker:
     def __init__(self, filename):
         self.filename = filename
-        self.read_file()
+        self.lines = self.read_file()
 
     def read_file(self):
         with open(self.filename, "r") as file:
             return file.read().split("\n")
 
     def get_domains(self):
-        lines = self.read_file()
-        lines = [line.replace(".", "") for line in lines]
-        return lines
+        self.lines = [line.replace(".", "") for line in self.lines]
+        return self.lines
 
     def get_names(self):
-        lines = self.read_file()
-        lines = [line.split("\t")[1] for line in lines]
-        return lines
+        self.lines = [line.split("\t")[1] for line in self.lines]
+        return self.lines
 
     def get_dates(self):
         dates_dict = []
-        lines = self.read_file()
-        for line in lines:
+        for line in self.lines:
             my_str = line.split(" - ")
             if len(my_str) > 1:
                 date = my_str[0]
@@ -40,20 +37,20 @@ class FilesWorker:
 
 # для самопроверки
 
-# filename = "domains.txt"
-# files_worker = FilesWorker(filename)
-# domains = files_worker.get_domains()
-# print(domains)
-#
-# filename = 'names.txt'
-# files_worker = FilesWorker(filename)
-# names = files_worker.get_names()
-# print(names)
-#
-# filename = 'authors.txt'
-# files_worker = FilesWorker(filename)
-# dates = files_worker.get_dates()
-# print(dates)
+filename = "domains.txt"
+files_worker = FilesWorker(filename)
+domains = files_worker.get_domains()
+print(domains)
+
+filename = 'names.txt'
+files_worker = FilesWorker(filename)
+names = files_worker.get_names()
+print(names)
+
+filename = 'authors.txt'
+files_worker = FilesWorker(filename)
+dates = files_worker.get_dates()
+print(dates)
 
 
 ##################################################################
