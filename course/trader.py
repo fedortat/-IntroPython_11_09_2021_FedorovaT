95c86ed91e41a1a7e83e741dcda681cf5c7da23a
fedtat <fedorova.int@gmail.com>
2021-12-04 14:00:20 +0200

    updated course work

--- course/trader.py
+++ course/trader.py
@@ -14,14 +14,15 @@ def get_data_from_config():
     return data
 
 
-# сохраняем стартовый баланс в csv в первой строке
-start_balance = get_data_from_config()
-start_balance = {key: start_balance[key] for key in start_balance.keys() & {"rate", "usd_balance", "uah_balance"}}
-with open(status, "w", encoding="utf-8") as csv_file:
-    fieldnames = ["rate", "usd_balance", "uah_balance"]
-    writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-    writer.writeheader()
-    writer.writerow(start_balance)
+# сохраняем стартовый баланс в csv в первой строке, а также используется для рестарта программы
+def start_restart():
+    start_balance = get_data_from_config()
+    start_balance = {key: start_balance[key] for key in start_balance.keys() & {"rate", "usd_balance", "uah_balance"}}
+    with open(status, "w", encoding="utf-8") as csv_file:
+        fieldnames = ["rate", "usd_balance", "uah_balance"]
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerow(start_balance)
 
 
 # генерируем случайный курс
@@ -109,7 +110,10 @@ def sell_all_usd():
     return log_balance_to_csv(result_s)
 
 
+start = start_restart()
+
 # операции для самопроверки:
+# start = start_restart()
 # amount = 150
 # new_balance = buy_usd(amount)
 # amount = 200
@@ -121,29 +125,62 @@ def sell_all_usd():
 # new_balance = sell_usd(amount)
 # new_balance = sell_all_usd()
 # new_balance = buy_all_usd()
+# restart = start_restart()
 
 
 parser = ArgumentParser()
-subparsers = parser.add_subparsers()
+subparsers = parser.add_subparsers(dest="command")
+rate = subparsers.add_parser("RATE")
+available = subparsers.add_parser("AVAILABLE")
+buy = subparsers.add_parser("BUY")
+sell = subparsers.add_parser("SELL")
+buy_all = subparsers.add_parser("BUY ALL")
+sell_all = subparsers.add_parser("SELL ALL")
+next = subparsers.add_parser("NEXT")
+restart = subparsers.add_parser("RESTART")
 
-parser_rate = subparsers.add_parser("rate", help="Getting current exchange rate")
-parser_rate.set_defaults(func=get_rate)
-
-parser_balance = subparsers.add_parser("available", help="Getting current account balance")
-parser_balance.set_defaults(func=get_balance)
-
-parser_buy = subparsers.add_parser("buy", help="Buying USD")
-parser_buy.add_argument("amount", dest="amount")
-parser_buy.set_defaults(func=buy_usd)
-
-parser_buy_all = subparsers.add_parser("buy all", help="Buying USD")
-parser_buy_all.set_defaults(func=buy_all_usd)
-
-parser_sell = subparsers.add_parser("sell", help="Buying USD")
-parser_sell.add_argument("amount", dest="amount")
-parser_sell.set_defaults(func=sell_usd)
-
-parser_sell_all = subparsers.add_parser("sell all", help="Buying USD")
-parser_sell_all.set_defaults(func=sell_all_usd)
+buy.add_argument("amount", nargs=1)
+sell.add_argument("amount", nargs=1)
 
 args = parser.parse_args()
+
+if args.command == "RATE":
+    res_rate = get_rate()
+    print(res_rate)
+elif args.command == "AVAILABLE":
+    get_balance()
+elif args.command == "BUY":
+    buy_usd()
+elif args.command == "SELL":
+    sell_usd()
+elif args.command == "BUY ALL":
+    buy_all_usd()
+elif args.command == "SELL ALL":
+    sell_all_usd()
+elif args.command == "NEXT":
+    get_rate()
+elif args.command == "RESTART":
+    start_restart()
+
+
+# parser_rate = subparsers.add_parser("rate", help="Getting current exchange rate")
+# parser_rate.set_defaults(func=get_rate)
+#
+# parser_balance = subparsers.add_parser("available", help="Getting current account balance")
+# parser_balance.set_defaults(func=get_balance)
+#
+# parser_buy = subparsers.add_parser("buy", help="Buying USD")
+# parser_buy.add_argument("amount", required=True, dest="amount")
+# parser_buy.set_defaults(func=buy_usd)
+#
+# parser_buy_all = subparsers.add_parser("buy all", help="Buying USD")
+# parser_buy_all.set_defaults(func=buy_all_usd)
+#
+# parser_sell = subparsers.add_parser("sell", help="Buying USD")
+# parser_sell.add_argument("amount", required=True, dest="amount")
+# parser_sell.set_defaults(func=sell_usd)
+#
+# parser_sell_all = subparsers.add_parser("sell all", help="Buying USD")
+# parser_sell_all.set_defaults(func=sell_all_usd)
+
+# args = parser.parse_args()
